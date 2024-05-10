2bdfe2344419a8a5be6f2063c920bc7e98430ab5
FALLOUT7744 <xunhaig@gmail.com>
2024-05-10 22:41:47 +0900

    'Todoの物理削除機能の実装'

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,4 +16,5 @@ Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
-route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::delete('/todo/{id}/delete', 'TodoController@delete')->name('todo.delete');
